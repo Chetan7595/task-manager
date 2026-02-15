bd7914705663c77b6bcde975bccabe4ee43fb277
Chetan <ck32266@gmail.com>
2026-02-15 23:46:21 +0530

    Add Code structure

---
+++ internal/models/task.go
@@ -0,0 +1,11 @@
+package model
+
+type Task struct {
+	Id          string
+	Title       string
+	Description string
+	Status      string
+	Priority    string
+	Due_Date    string
+	User_Id     string
+}
